c5b5b6eb5135ef75ca85b771fe5d8c2533247de6
Seth Hochberg <seth@sethhochberg.com>
2013-08-28 10:01:02 -0700

    init with sane gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inkreactor::Application.config.secret_key_base = 'fd9a2d19ebb24198f336dc7484f2c05bdb0b806e2f790e8e58a05019564ebaa680b343e2124547fd5f103bc7f4fbf062df1a60497cf35566972611f9c0c38c5d'
