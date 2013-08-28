187c640766c8904be34e612f5a1ca15f5b0dba18
dbcapprentice <dbcapprentice@dbc.com>
2013-08-28 11:16:52 -0700

    tests mock for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe User do
+  context "user database operations" do
+
+    describe "user should be persisted in the database" do
+      pending
+    end
+
+  end
+
+  context "user model functionality" do
+    
+  end
+end
