7ca26ad44d88f9e0e403e4d08b0d6d9d13f4e825
dbcapprentice <dbcapprentice@dbc.com>
2013-08-28 14:07:53 -0700

    create model and migration for comments

---
+++ db/migrate/20130828210420_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :content
+
+      t.belongs_to :user
+      t.belongs_to :post
+
+      t.timestamps
+    end
+  end
+end
