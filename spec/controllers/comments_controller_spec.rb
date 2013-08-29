ddd1ae8b9ec70319ab5d08418e1dc66010460428
Apprentice <apprentice@dbc24.local>
2013-08-29 14:21:53 -0700

    user should not be able to create a blank comment. add test for redirect to post show page after comment.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -19,6 +19,28 @@ describe CommentsController do
           comment_action
           }.to change(Comment, :count).by(1)
       end
+
+      it "redirects to post's show page" do
+        comment_action
+        response.should redirect_to post_path(post_to_comment_on)
+      end
     end
+
+    context 'with invalid attributes' do
+      let(:post_to_comment_on) { FactoryGirl.create(:post) }
+      let(:comment_action) { post :create, post_id: post_to_comment_on.id, comment: { content: '' } }
+
+      it 'does NOT create a new comment' do
+        expect {
+          comment_action
+        }.to_not change(Comment, :count)
+      end
+
+      it "redirects to post's show page" do
+        comment_action
+        response.should redirect_to post_path(post_to_comment_on)
+      end
+    end
+    
   end
 end
