0b547a361a40fdce482142dfb9f5f57d18f67ac2
Steven Harlow <STHarlow@gmail.com>
2013-08-29 23:56:50 -0700

    change devise redirect after sign-in to posts#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password)}
   end
+
+  #overrides devise redirect after sign-in
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
 end
