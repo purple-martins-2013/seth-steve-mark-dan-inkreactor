226db201afd919c8024b18f0229d2441e79193a8
Steven Harlow <STHarlow@gmail.com>
2013-08-30 16:31:59 -0700

    javascript specs

---
+++ spec/javascripts/edit_comment_view.rb
@@ -0,0 +1,12 @@
+function AddEditWidget() {
+
+}
+
+describe("AddEditWidget", function() {
+  it("opens the response from ajax:success as a modal in page", function() {
+    var editComment = function() {
+      $('#')
+    }
+
+  });
+});
