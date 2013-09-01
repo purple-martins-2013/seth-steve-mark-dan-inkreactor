8ba29923695b4ff328b9f9a4ec8df919581bf785
mehagel65@gmail.com <mehagel65@gmail.com>
2013-09-01 16:55:10 -0700

    vote model and migration

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,7 +5,6 @@ describe Vote do
 	context 'vote model functionality' do
 		it {should validate_presence_of (:user_id)}
 		it {should validate_presence_of (:post_id)}
-		it {should validate_presence_of (:upvote)}
 	end
   
 end
